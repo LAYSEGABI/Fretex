9b73ef18ed059bd63f9326257f9395b8da33552a
LAYSEGABI <laysegabrielly13@gmail.com>
2023-07-08 09:22:00 -0300

    site att

---
+++ main.js
@@ -0,0 +1,20 @@
+//Animacao de entrada e saida de conteudo ao rolar a página
+const elementos = document.querySelectorAll('[data-anima]');
+const animacaoClass = 'animacao';
+
+function animaScroll() {
+  const topoPaginaNaJanela = window.pageYOffset + ((window.innerHeight * 3 / 4));
+  elementos.forEach(function (elemento) {
+    if (topoPaginaNaJanela > elemento.offsetTop) {
+      elemento.classList.add(animacaoClass);
+    } else {
+      elemento.classList.remove(animacaoClass);
+    }
+});
+}
+
+if(elementos.length){
+  window.addEventListener('scroll', function(){
+    animaScroll()
+  })
+}
