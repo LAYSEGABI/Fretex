9027120b8dbd056716db22ac8737f839b111f47a
LAYSEGABI <laysegabrielly13@gmail.com>
2023-07-15 21:33:41 -0300

    header e banner responsivos

--- main.js
+++ main.js
@@ -18,3 +18,12 @@ if(elementos.length){
     animaScroll()
   })
 }
+
+
+
+// Menu hamburguer
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
